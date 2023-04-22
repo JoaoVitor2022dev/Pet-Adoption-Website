c37060d2d51c2a3765c55b894caba36e228c89d0
joao vitor <githubconta2021@gmail.com>
2023-04-22 01:02:08 -0400

    titulo: validaçao de usuario
    1 - validando os dados preenchidos
    2 - verificando confirmaçao de senha
    3 - (check email ), usuario usando apenas um email

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,43 @@ const User = require('../models/User');
 
 module.exports = class UserController {
     static async register( req , res ) {
-         res.json('Ola Get a Pet');
+          
+    const { name , email , phone , password, confirmpassword } = req.body; 
+
+    // validations 
+    if(!name){
+       res.status(422).json({ message: "O nome é obrigatorio" });
+       return 
+    }
+    if(!email){
+        res.status(422).json({ message: "O Email é obrigatorio" });
+        return 
+    }
+    if(!phone){
+        res.status(422).json({ message: "O telefone é obrigatorio" });
+         return 
+    }
+    if(!password){ 
+       res.status(422).json({ message: "A senha é obrigatorio"});
+    }
+    if(!confirmpassword){
+        res.status(422).json({ message: "A confirmaçao de senha é obrigatorio" });
+         return 
+     }
+
+    // conferir se a senha esta correta 
+
+    if(password !== confirmpassword){
+      res.status(422).json({ message: "A senha e a confirmaçao de senha precisam ser iguais!" })
+      return
+    } 
+    // 2 email iguais
+
+    const userExists = await User.find({email: email}) 
+
+    if (userExists) {
+      res.status(422).json({ message: "Por favor, utilize outro E-mail"});
+      return
+    }
     }
 }
