8b207e57297ce333064bcdca6b2978cbdd30e0b0
joao vitor <githubconta2021@gmail.com>
2023-05-06 16:33:59 -0400

    Titulo: Removendo Pet Do Sistema
    1 - ao criar a rota, colocamos um verificaçao de webtoke, apenas quem é dono do pet pode rmover ele de lá;
    2 - verificaçoes da fucntion do controller dos pets;
    3 - validaçao de id invalido;
    4 - validaçao de id que nao existe;
    5 - validaçao de pet.user._id e user._id, para apenas quem é dono do pet poder removelo;

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -110,6 +110,7 @@ module.exports = class PetController {
        
         const id = req.params.id
 
+          // check if id is valid
         if (!ObjectId.isValid(id)) {
             res.status(422).json({ message: 'ID invalido!' }); 
         }
@@ -122,5 +123,38 @@ module.exports = class PetController {
         }
 
         res.status(200).json({ pet: pet })
+    }
+
+    static async removePetById(req, res) {
+      
+        const id = req.params.id; 
+
+        // check if id is valid
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'ID inválido!' })
+            return
+        }
+
+        // check exist a pet 
+        const pet = await Pet.findOne({ _id: id }); 
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet nao encontrado' })
+        }
+  
+
+        // check if logged in user registered the pet 
+
+        const token = getToken(req); 
+        const user = await getUserByToken(token)
+ 
+        if(pet.user._id.toString() !== user._id.toString()){ 
+          res.status(422).json({ message: 'Houve um problema em processar a sua solicitaçao, tente mais tarde !'});
+        }
+ 
+        await Pet.findByIdAndRemove(id);
+
+        res.status(200).json({ message: "Pet temovido com sucesso!" })
+
     }
 }
--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -10,6 +10,7 @@ router.get('/', PetController.getAll);
 router.get('/mypets', verifyToken, PetController.getAllUserPets); 
 router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions); 
 router.get('/:id', PetController.getPetById);
+router.delete('/:id', PetController.removePetById);
 
 module.exports = router 
 
