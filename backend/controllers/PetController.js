5a2dc819c64674cac328d64be676530f3b94a082
joao vitor <githubconta2021@gmail.com>
2023-05-06 00:13:49 -0400

    titulo: Rotas de Pets
    1 - construimos a rota que resgata todos os Pets;
    2 - desenvolvemos tambem a rota de Pets que estam disponiveis para adoçao;
    3 - ultima rota, que é de acordo com o id do pet, rota de pet individual;
    4 - recebendo dados de fotos de pets e perfils;

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -3,6 +3,7 @@ const Pet = require('../models/Pet');
 // helps 
 const getToken = require('../helpers/get-toke');
 const getUserByToken = require('../helpers/get-user-by-token');
+const ObjectId = require('mongoose').Types.ObjectId
 
 module.exports = class PetController {
     // create a pet 
@@ -52,11 +53,11 @@ module.exports = class PetController {
             available,
             images: [],
             user: {
-              _id: user.id,
-              name: user.name,
-              Image: user.image,
-              phone: user.phone,
-            },
+                _id: user._id,
+                name: user.name,
+                image: user.image,
+                phone: user.phone,
+              },
         })
  
         images.map((image) => {
@@ -72,5 +73,54 @@ module.exports = class PetController {
         } catch (err) {
             res.status(500).json({message: err})
         }
+    }
+
+    static async getAll(req, res){ 
+       
+        const pets = await Pet.find().sort('-createdAt')
+
+        res.status(200).json({ pets: pets});
+    }
+
+    // get all user pets
+     static async getAllUserPets(req, res) {
+       const token = getToken(req)
+       const user = await getUserByToken(token)
+
+       console.log(user);
+
+       console.log(user._id);
+
+       const pets = await Pet.find({ "user._id": user._id }).sort('-createdAt')
+
+        res.status(200).json({ pets })
+    }
+
+    // pets para a adoçao
+    static async getAllUserAdoptions(req , res){
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+ 
+        const pets = await Pet.find({ "adopter._id": user._id }).sort('-createdAt')
+ 
+        res.status(200).json({ pets })
+    }
+
+    static async getPetById(req, res ) {
+       
+        const id = req.params.id
+
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'ID invalido!' }); 
+        }
+
+        // get pat by id 
+        const pet = await Pet.findOne({ _id: id });
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet nao encontrado!' })
+        }
+
+        res.status(200).json({ pet: pet })
     }
 }
--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -6,6 +6,10 @@ const verifyToken = require("../helpers/verify-token");
 const { imageUpload } = require("../helpers/image-upload");
 
 router.post('/create', verifyToken , imageUpload.array("images"), PetController.create); 
+router.get('/', PetController.getAll);
+router.get('/mypets', verifyToken, PetController.getAllUserPets); 
+router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions); 
+router.get('/:id', PetController.getPetById);
 
 module.exports = router 
 
