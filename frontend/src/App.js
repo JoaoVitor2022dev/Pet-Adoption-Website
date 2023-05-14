cb7222014e7e0bc187d0c2e117768ec2f5a11449
joao vitor <githubconta2021@gmail.com>
2023-05-14 16:38:33 -0400

    Titulo: Criando Context API e encapsulando na apicaçao
    1 - criar o Context no UseContext.js ;
    2 - Importar no app.js e acapsular toda a aplicaçao ;
    3-  esparar o contexto ;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
+// componentes de react router DOM
 import { BrowserRouter, Routes , Route, Navigate } from "react-router-dom"; 
+
+// Context API
+import { UserProvider } from "./context/UserContext";
 
 // componentes navbar e footer
 import Footer from "./components/layout/Footer";
@@ -10,19 +14,22 @@ import Home from "../src/components/Home";
 import Login from "../src/components/pages/auth/Login";
 import Register from "../src/components/pages/auth/Register";
 
+
 function App() {
   return (
-     <BrowserRouter>
-      <Navbar/>
-      <Container>
-      <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/login" element={<Login/>}/>
-       </Routes>
-      </Container>
-       <Footer/>
-     </BrowserRouter>
+     <UserProvider>
+        <BrowserRouter>
+          <Navbar/>
+          <Container>
+          <Routes>
+             <Route path="/" element={<Home/>}/>
+             <Route path="/register" element={<Register/>}/>
+             <Route path="/login" element={<Login/>}/>
+          </Routes>
+          </Container>
+          <Footer/>
+       </BrowserRouter>
+     </UserProvider>
   );
 }
 
--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -18,7 +18,7 @@ export default function useAuth() {
          console.log(error);
         }
     }
-
     return { register }
 }
+
 
